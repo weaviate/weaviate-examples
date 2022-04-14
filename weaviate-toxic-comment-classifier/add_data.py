89369ff88dccfd8e1b0faf5f8fa05167eb51d449
YASH-GU24 <yashg.it.20@nitj.ac.in>
2022-04-14 11:34:34 +0530

    Added a example using toxicity classification dataset having a small GUI using Tkinter library of python

---
+++ weaviate-toxic-comment-classifier/add_data.py
@@ -0,0 +1,45 @@
+import weaviate
+import pandas as pd
+
+#setting up client
+client = weaviate.Client("http://localhost:8080")
+
+client.schema.delete_all()
+#creating the schema
+comment_schema = {
+    "class": "Comments",
+    "description": "comments of people",
+    "properties": [
+        {
+            "name": "comment",
+            "dataType": ["string"],
+            "description": "The text of the comment", 
+        },
+        {
+            "name": "label",
+            "dataType": ["string"],
+            "description": "The label of the comment", 
+        }
+    ]
+}
+client.schema.create_class(comment_schema)
+
+#loading the dataset
+data=pd.read_csv("./train.csv")
+
+#shuffling the datset
+data=data.sample(frac=1)
+
+#adding data to weaviate
+#Adding only 1000 entries, you can increase or decrease the number of entries according to time
+for i in range (0,1000):
+    lab=""
+    if(data.iloc[i][1]==1):
+        lab="toxic"
+    else:
+        lab="Non Toxic"
+    obj = {
+                "comment": str(data.iloc[i][0]),
+                "label": lab
+          }
+    client.data_object.create(obj, "Comments")

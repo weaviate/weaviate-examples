9245d448bb24d647c4886575c6b39b2d129374b3
Aakash Thatte <aathatte2002@gmail.com>
2022-05-31 14:53:51 +0530

    Add code to return zero vector if no face found in an image

--- attendance-system-example/own_vec.py
+++ attendance-system-example/own_vec.py
@@ -2,7 +2,7 @@ import face_recognition
 import pickle
 import cv2
 import os
-
+import numpy as np
 # ip = "students/Aakash/1.jpg"
 
 # image = cv2.imread(ip)
@@ -23,5 +23,11 @@ def getFaceEncoding(image_path):
     rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     boxes = face_recognition.face_locations(rgb,model='hog')
     encodings = face_recognition.face_encodings(rgb, boxes)
+
+    # If no face vector could be extracted, return an array of zeros.
+    if(len(encodings)==0):
+        return np.zeros(128)
+
+    # print("Length of face encoding:",len(encodings),"and length of vector:",len(encodings[0]))
     return encodings[0]
 

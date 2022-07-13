79f50a40fcc6b015fdd1b0c961d24be23ae5d11f
Sebastian Witalec <sebawita@users.noreply.github.com>
2022-07-13 14:45:50 +0200

    improvements to the import process

--- movies-search-engine/add_data.py
+++ movies-search-engine/add_data.py
@@ -1,21 +1,20 @@
 #importing required libraries
 import weaviate
 import pandas as pd
+import math
 #setting up client
 client = weaviate.Client("http://localhost:8080")
 
-
-
-#Checking if Movies schema is present, If it is already present deleting it
-for i in range (0,len(client.schema.get()['classes'])):
-    if client.schema.get()['classes'][i]['class']=='Movies':
+#Checking if Movies schema already exists, then delete it
+current_schemas = client.schema.get()['classes']
+for schema in current_schemas:
+    if schema['class']=='Movies':
         client.schema.delete_class('Movies')
-
 
 #creating the schema
 movie_class_schema = {
     "class": "Movies",
-    "description": "Various Info about plants",
+    "description": "A collection of movies with title, description, director, actors, rating, etc.",
     "properties": [
         {
             "name": "movie_id",
@@ -71,17 +70,7 @@ movie_class_schema = {
             "name": "rating_count",
             "dataType": ["number"],
             "description": "rating count of the movie", 
-        },        
-        {
-            "name": "best_rating",
-            "dataType": ["number"],
-            "description": "best rating of the movie", 
-        },        
-        {
-            "name": "worst_rating",
-            "dataType": ["number"],
-            "description": "worst rating of the movie", 
-        },        
+        }, 
         {
             "name": "rating_value",
             "dataType": ["number"],
@@ -91,56 +80,58 @@ movie_class_schema = {
             "name": "review_aurthor",
             "dataType": ["string"],
             "description": "aurthor of the review", 
-        },  
-        {
-            "name": "review_date",
-            "dataType": ["string"],
-            "description": "date of review", 
         },
         {
             "name": "review_body",
             "dataType": ["string"],
             "description": "body of the review", 
-        },
-        {
-            "name": "duration",
-            "dataType": ["string"],
-            "description": "the duration of the review", 
-        },
+        }
     ]
 }
 client.schema.create_class(movie_class_schema)
 
-#change the path to the place where data csv file is present
+#Configure batch process - for faster imports 
+#see: https://weaviate.io/developers/weaviate/current/restful-api-references/batch.html
+client.batch.configure(
+  batch_size=10, 
+  # dynamically update the `batch_size` based on import speed
+  dynamic=True,
+  timeout_retries=3,
+)
+
+#Change the path to the place where data csv file is present
 data=pd.read_csv("final_data.csv")
 
-#adding the data
+#Adding the data
 #You can decrease number of objects to be inserted to decrease the amount of time required
 for i in range (0,len(data)):
-            movie_object = {
-                'movie_id':float(data.iloc[i]['id']),
-                'url':str(data.iloc[i]['url']),
-                'title': str(data.iloc[i]['Name']).lower(),
-                'poster_link': str(data.iloc[i]['PosterLink']),
-                'genres':str(data.iloc[i]['Genres']),
-                'actors': str(data.iloc[i]['Actors']).lower(),
-                'director': str(data.iloc[i]['Director']).lower(),
-                'description':str(data.iloc[i]['Description']),
-                'date_published': str(data.iloc[i]['DatePublished']),
-                'keywords': str(data.iloc[i]['Keywords']),
-                'rating_count':float(data.iloc[i]['RatingCount']),
-                'best_rating': float(data.iloc[i]['BestRating']),
-                'worst_rating': float(data.iloc[i]['WorstRating']),
-                'rating_value':float(data.iloc[i]['RatingValue']),
-                'review_aurthor': str(data.iloc[i]['ReviewAurthor']),
-                'review_date': str(data.iloc[i]['ReviewDate']),
-                'review_body': str(data.iloc[i]['ReviewBody']),
-                'duration': str(data.iloc[i]['duration']),
-            }
+    item = data.iloc[i]
 
-            try:
-                client.data_object.create(movie_object, "Movies")
-            except:
-                print("failed",i)
+    movie_object = {
+        'movie_id':float(item['id']),
+        'url':str(item['url']),
+        'title': str(item['Name']).lower(),
+        'poster_link': str(item['PosterLink']),
+        'genres':str(item['Genres']),
+        'actors': str(item['Actors']).lower(),
+        'director': str(item['Director']).lower(),
+        'description':str(item['Description']),
+        'date_published': str(item['DatePublished']),
+        'keywords': str(item['Keywords']) if str(item['Keywords']) != "nan" else "",
+        'rating_count': float(item['RatingCount']) if not math.isnan(float(item['RatingCount'])) else -1,
+        'rating_value':float(item['RatingValue']) if not math.isnan(float(item['RatingValue'])) else -1,
+        'review_aurthor': str(item['ReviewAurthor']) if str(item['ReviewAurthor']) != "nan" else "",
+        'review_body': str(item['ReviewBody']) if str(item['ReviewBody']) != "nan" else "",
+    }
 
-            print(str(i)+"/"+str(len(data)))
+    try:
+        client.batch.add_data_object(movie_object, "Movies")
+    except BaseException as error:
+        print("Import Failed at: ",i)
+        print("An exception occurred: {}".format(error))
+        # Stop the import on error
+        break
+
+    print("Status: ", str(i)+"/"+str(len(data)))
+
+client.batch.flush()

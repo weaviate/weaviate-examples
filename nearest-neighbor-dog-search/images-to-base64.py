a8f97ce12f7be049c27b7bacd9a3b485b96b6f78
Erika Cardenas <110841617+erika-cardenas@users.noreply.github.com>
2022-10-14 10:34:06 -0400

    fixed base64 script

--- nearest-neighbor-dog-search/images-to-base64.py
+++ nearest-neighbor-dog-search/images-to-base64.py
@@ -4,8 +4,9 @@ import base64
 # Do NOT run this file. The images have already been converted and are in the base64_images folder
 
 os.mkdir("base64_images") # create the base64_images folder 
+img_path = "./flask-app/static/img/"
 
-for file_path in os.listdir("./flask-app/static/img"): # grabbing the images in the images folder and converting them to base64
+for file_path in os.listdir(img_path): # grabbing the images in the images folder and converting them to base64
     if ".DS_Store" not in file_path:
         filename = file_path.replace(".jpg", "").split("/")[-1]
-        os.system("cat " + "images/" + file_path + " | base64 > base64_images/" + filename + ".jpg")
+        os.system("cat " + img_path + file_path + " | base64 > base64_images/" + filename + ".jpg")

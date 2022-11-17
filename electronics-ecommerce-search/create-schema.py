59afb38cb6b4a38dd11d8b8b2b1ad60b16a049f4
laura-ham <lauraham96@gmail.com>
2022-11-17 14:22:24 +0100

    improve scripts

--- electronics-ecommerce-search/create-schema.py
+++ electronics-ecommerce-search/create-schema.py
@@ -18,51 +18,111 @@ schema = {
             "description": "Electronics product",
             "vectorIndexType": "hnsw", 
             "vectorizer": "text2vec-transformers",
+            "moduleConfig": {
+                "text2vec-transformers": {
+                    "poolingStrategy": "masked_mean",
+                    "vectorizeClassName": False
+                }
+            },
             "properties": [
                 {
                     "name": "productId",
                     "dataType": ["string"],
                     "description": "id",
+                    "moduleConfig": {
+                        "text2vec-transformers": {
+                            "skip": True,
+                            "vectorizePropertyName": False
+                        }
+                    },
                 },
                 {
                     "name": "name",
                     "dataType": ["text"],
                     "description": "name of the product",
+                    "moduleConfig": {
+                        "text2vec-transformers": {
+                            "skip": False,
+                            "vectorizePropertyName": False
+                        }
+                    },
                 },
                 {
                     "name": "title",
                     "dataType": ["text"],
                     "description": "title of the product containing the supplier",
+                    "moduleConfig": {
+                        "text2vec-transformers": {
+                            "skip": False,
+                            "vectorizePropertyName": False
+                        }
+                    },
                 },
                 {
                     "name": "description",
                     "dataType": ["text"],
                     "description": "short description",
+                    "moduleConfig": {
+                        "text2vec-transformers": {
+                            "skip": False,
+                            "vectorizePropertyName": False
+                        }
+                    },
                 },
                 {
                     "name": "imageLink",
                     "dataType": ["string"],
                     "description": "link to an image of the product",
+                    "moduleConfig": {
+                        "text2vec-transformers": {
+                            "skip": True,
+                            "vectorizePropertyName": False
+                        }
+                    },
                 },
                 {
                     "name": "ean",
                     "dataType": ["string"],
                     "description": "unique product code ean",
+                    "moduleConfig": {
+                        "text2vec-transformers": {
+                            "skip": True,
+                            "vectorizePropertyName": False
+                        }
+                    },
                 },
                 {
                     "name": "releaseDate",
                     "dataType": ["date"],
                     "description": "date released",
+                    "moduleConfig": {
+                        "text2vec-transformers": {
+                            "skip": True,
+                            "vectorizePropertyName": False
+                        }
+                    },
                 },
                 {
                     "name": "supplier",
                     "dataType": ["Supplier"],
                     "description": "supplier",
+                    "moduleConfig": {
+                        "text2vec-transformers": {
+                            "skip": False,
+                            "vectorizePropertyName": False
+                        }
+                    },
                 },
                 {
                     "name": "price",
                     "dataType": ["number"],
                     "description": "price of the product",
+                    "moduleConfig": {
+                        "text2vec-transformers": {
+                            "skip": True,
+                            "vectorizePropertyName": False
+                        }
+                    },
                 }
             ]
         }, {
@@ -75,6 +135,12 @@ schema = {
                     "name": "name",
                     "dataType": ["string"],
                     "description": "name of the supplier",
+                    "moduleConfig": {
+                        "text2vec-transformers": {
+                            "skip": False,
+                            "vectorizePropertyName": False
+                        }
+                    },
                 },
             ]
         },
